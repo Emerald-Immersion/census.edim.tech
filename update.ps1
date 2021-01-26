7cbb7814c591c36b536315a3b3b438abae790923
SkAzZ <github@skazz.io>
2021-01-26 23:57:32 +0000

    Added initial examples and web page
    Excluded characters/leaderboard/outfits

---
+++ update.ps1
@@ -0,0 +1,27 @@
+
+Import-Module ./src/PSDayBreak.psm1
+
+# TODO: Run updates...
+
+
+# Get-JsonIndex -Path './docs/data' | ConvertTo-Json -Depth 10 | Out-File './docs/example/index.json'
+
+
+Get-JsonIndex -Path './docs/example' | ConvertTo-Json -Depth 10 | Out-File './docs/example/index.json'
+
+Function Get-JsonIndex ($Path) {
+    $ht = [ordered]@{}
+
+    Get-ChildItem -Path $Path -Exclude 'index.json' | ForEach-Object {
+        if ($_.PSIsContainer) {
+            $ht["/$($_.Name)"] = Get-JsonIndex -Path $_.FullName
+        } else {
+            $ht[$_.Name] = [ordered]@{
+                Length = $_.Length
+                LastWriteTimeUtc = [DateTimeOffset]::new($_.LastWriteTimeUtc).ToUnixTimeMilliseconds()
+            }
+        }
+    }
+
+    [pscustomobject]$ht
+}
